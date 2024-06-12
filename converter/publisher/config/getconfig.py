0b71886b4b226d4943eb9c3ff0fe41d04dfe2cc0
QuincyRom <129735903+QuincyRom@users.noreply.github.com>
2024-06-12 14:47:03 +0200

    adjustments to be the source script

--- converter/publisher/config/getconfig.py
+++ converter/publisher/config/getconfig.py
@@ -3,4 +3,6 @@ import yaml
 def getConfig():
     with open("./converter/publisher/config/config.yaml", "r") as yamlFileConfig:
         return yaml.safe_load(yamlFileConfig)
-        
+    if overrides:
+        config.update(overrides)
+    return config
