bacee87efd1b43a7ac14cfc3e31c07d4b0b526ab
QuincyRom <129735903+QuincyRom@users.noreply.github.com>
2024-10-10 16:07:45 +0200

    Update main.py

--- converter/publisher/main.py
+++ converter/publisher/main.py
@@ -7,29 +7,22 @@ from pagesPublisher import publishFolder
 
 logging.basicConfig(level=logging.INFO)
 
-# Parse arguments
+
+# Parse arguments with LOGIN and PASSWORD for Confluence
 parser = argparse.ArgumentParser()
 parser.add_argument('--login', help='Login with "" is mandatory', required=True)
-parser.add_argument('--password', help='Password with "" is mandatory', required=True)
-parser.add_argument('--folder', help='Folder containing markdown files', required=True)
-parser.add_argument('--confluence_space', help='Confluence space key', required=True)
-parser.add_argument('--parent_page_id', help='Confluence parent page ID', required=True)
+parser.add_argument('--password', help='Password with "" is mandatory',  required=True)
 args = parser.parse_args()
 inputArguments = vars(args)
 
-overrides = {
-    "github_folder_with_md_files": inputArguments['folder'],
-    "confluence_space": inputArguments['confluence_space'],
-    "counfluence_parent_page_id": inputArguments['parent_page_id']
-}
 
-CONFIG = getConfig(overrides=overrides)
+CONFIG = getConfig()
 
 logging.debug(CONFIG)
 
 pages = searchPages(login=inputArguments['login'], password=inputArguments['password'])
 deletePages(pagesIDList=pages, login=inputArguments['login'], password=inputArguments['password'])
 
-publishFolder(folder=CONFIG["github_folder_with_md_files"], 
-              login=inputArguments['login'], 
-              password=inputArguments['password'])
+publishFolder(folder = str(CONFIG["github_folder_with_md_files"]), 
+  login=inputArguments['login'], 
+  password=inputArguments['password'])
